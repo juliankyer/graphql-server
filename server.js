b922a8a04afcdc207ba4156bd4516b17a0f4e5b2
Julian Kyer <julian.kyer@cognizant.com>
2017-11-21 14:58:34 -0700

    Broken on pg 650

--- server.js
+++ server.js
@@ -33,8 +33,27 @@ const RootQuery = new GraphQLObjectType({
           type: new GraphQLNonNull(GraphQLID)
         }
       },
-      resolve(source, args) {
-        return loaders.getNodeById(args.id);
+      resolve(source, args, context, info) {
+        let includeFriends = false;
+
+        const selectionFragments = info.fieldASTs[0].selectionSet.selections;
+        const userSelections = selectionFragments.filter(selection => {
+          return selection.kind === 'InlineFragment' && selection.typeCondition.name.value === 'User';
+        })
+
+        userSelections.forEach(selection => {
+          selection.selectionSet.selections.forEach(innerSelection => {
+            if (innerSelection.name.value === 'friends') {
+              includeFriends = true;
+            }
+          });
+        });
+
+        if (includeFriends) {
+          return loaders.getUserNodeWithFriends(args.id);
+        } else {
+          return loaders.getNodeById(args.id);
+        }
       }
     }
   }
--- src/types.js
+++ src/types.js
@@ -5,9 +5,12 @@ import {
   GraphQLString,
   GraphQLNonNull,
   GraphQLList,
+  GraphQLBoolean,
+  GraphQLInt,
 } from 'graphql';
 
 import * as tables from './tables';
+import * as loaders from './loaders';
 
 export const NodeInterface = new GraphQLInterfaceType({
   name: 'Node',
@@ -28,20 +31,105 @@ const resolveId = (source) => {
   return tables.dbIdToNodeId(source.id, source.__tableName);
 };
 
+const PageInfoType = new GraphQLObjectType({
+  name: 'PageInfo',
+  fields: {
+    hasNextPage: {
+      type: new GraphQLNonNull(GraphQLBoolean)
+    },
+    hasPreviousPage: {
+      type: new GraphQLNonNull(GraphQLBoolean)
+    },
+    startCursor: {
+      type: GraphQLString,
+    },
+    endCursor: {
+      type: GraphQLString,
+    }
+  }
+});
+
+const PostEdgeType = new GraphQLObjectType({
+  name: 'PostEdge',
+  fields: () => {
+    return {
+      cursor: {
+        type: new GraphQLNonNull(GraphQLString)
+      },
+      node: {
+        type: new GraphQLNonNull(PostType)
+      }
+    }
+  }
+});
+
+const PostsConnectionType = new GraphQLObjectType({
+  name: 'PostsConnection',
+  fields: {
+    pageInfo: {
+      type: new GraphQLNonNull(PageInfoType)
+    },
+    edges: {
+      type: new GraphQLNonNull(PostEdgeType)
+    }
+  }
+});
+
 export const UserType = new GraphQLObjectType({
   name: 'User',
   interfaces: [ NodeInterface ],
-  fields: {
-    id: {
-      type: new GraphQLNonNull(GraphQLID),
-      resolve: resolveId,
-    },
-    name: {
-      type: new GraphQLNonNull(GraphQLString)
-    },
-    about: {
-      type: new GraphQLNonNull(GraphQLString),
-    }
+  fields: () => {
+    return {
+      id: {
+        type: new GraphQLNonNull(GraphQLID),
+        resolve: resolveId
+      },
+      name: { type: new GraphQLNonNull(GraphQLString) },
+      about: { type: new GraphQLNonNull(GraphQLString) },
+      friends: {
+        type: new GraphQLList(UserType),
+        resolve(source) {
+          return loaders.getFriendIdsForUser(source).then(rows => {
+            const promises = rows.map(row => {
+              const friendNodeId = tables.dbIdToNodeId(row.user_id_b, row.__tableName);
+              return loaders.getNodeById(friendNodeId);
+            });
+            return Promise.all(promises);
+          })
+        }
+      },
+      posts: {
+        type: PostsConnectionType,
+        args: {
+          after: {
+            type: GraphQLString
+          },
+          first: {
+            type: GraphQLInt
+          },
+        },
+        resolve(source, args) {
+          return loaders.getPostIdsForUser(source, args).then(({ rows, pageIngfo }) => {
+            const promises = rows.map(row => {
+              const postNodeId = tables.dbIdToNodeId(row.id, row.__tableName);
+              return loaders.getNodeById(postNodeId).then(node => {
+                const edge = {
+                  node,
+                  cursor: row.__cursor,
+                };
+                return edge;
+              });
+            });
+            return Promise.all(promises).then(edges => {
+              return {
+                edges,
+                pageInfo
+              }
+            });
+          })
+        }
+      }
+    };
   }
 });
 
@@ -60,4 +148,4 @@ export const PostType = new GraphQLObjectType({
       type: new GraphQLNonNull(GraphQLString),
     }
   }
-})
+});
